83a7fabf3e11061c70143c3d77191be73a4a5f3e
Anatoly Kanaev <avloton@gmail.com>
2022-02-05 23:25:31 +0500

    Домашнее задание к занятию "5.4. Оркестрация группой Docker контейнеров на примере Docker Compose"

---
+++ homeWork/5.4/src/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1gr44d4i93lihefk8bs"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1goj6apkc57lq3tc4cf"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd808u8r1qchoiaeojvj"
+}
